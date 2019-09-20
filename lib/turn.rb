e4a74fc3c2ec80d74202cba8765c66f018bd70a6
Ronda D Cowen <cowenhome5@gmail.com>
2019-09-20 20:35:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-  input_to_index(user_input) = index
+ index = input_to_index(user_input) 
   if  valid_move(board) == true
     move(board, index, current_player)
   end
